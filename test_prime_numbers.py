583190524da951df2bbabc394a7c1e1958a7fcb7
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-03 15:10:02 +0300

    Write tests for when args are 0 or 5

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -2,9 +2,17 @@ import unittest
 from prime_numbers import generate_primes
 
 class PrimeNumbersTestCase(unittest.TestCase):
-    def test_non_integer_arg(self):
+    def test_non_integer_argument(self):
+        """Should raise a TypeRoor for non integer arg"""
         self.assertRaises(TypeError, generate_primes, 'hello')
 
+    def test_return_for_zero_argument(self):
+        """Should return an empty list for n = 0"""
+        self.assertEquals(generate_primes(0), [])
+
+    def test_return_when_arg_is_5(self):
+        """Should return [2, 3, 5] for n = 5"""
+        self.assertEquals(generate_primes(5), [2, 3, 5])
 
 if __name__ == '__main__':
     unittest.main()
