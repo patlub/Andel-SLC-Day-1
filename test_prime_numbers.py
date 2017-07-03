78425eb1e3a466416aa97a651eddb03c577189d0
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-03 15:20:54 +0300

    Write test for primes 0 - 2 and 0 - 10

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -14,5 +14,13 @@ class PrimeNumbersTestCase(unittest.TestCase):
         """Should return [2, 3, 5] for n = 5"""
         self.assertEquals(generate_primes(5), [2, 3, 5])
 
+    def test_return_when_arg_is_2(self):
+        """Should return [2] for n = 2"""
+        self.assertEquals(generate_primes(2), [2])
+
+    def test_return_when_arg_is_10(self):
+        """Should return [2, 3, 5, 7]"""
+        self.assertEquals(generate_primes(10), [2, 3, 5, 7])
+
 if __name__ == '__main__':
     unittest.main()
