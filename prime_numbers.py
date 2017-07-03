d262f49e6b394fae0eda1aab2d6140e45d1f5174
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-03 17:03:56 +0300

    Add time analysis and some comments

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,3 +1,5 @@
+import time
+
 def generate_primes(n) -> list:
     """
     Function to generate prime numbers form 0 to n
@@ -11,6 +13,13 @@ def generate_primes(n) -> list:
     if not isinstance(n, int):
         raise TypeError('Argument should be an integer')
 
+    elif n < 2:
+        return []
+
+    # Start Timer
+    start = time.time()
+
+    # initialize list to hold prime numbers
     primes = []
     for num in range(2, n + 1):
         for i in range(2, num):
@@ -18,4 +27,15 @@ def generate_primes(n) -> list:
                 break
         else:
             primes.append(num)
+
+    end = time.time()
+    print("The program ran for " + str(end-start))
     return primes
+
+
+generate_primes(10) # The program ran for 0.0
+generate_primes(100) # The program ran for 0.0
+generate_primes(1000) # The program ran for 0.0010271072387695312
+generate_primes(10000) # The program ran for 0.8187892436981201
+
+# Running time is O(n). Linear Running time
