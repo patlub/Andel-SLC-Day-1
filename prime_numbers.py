b42c34f6e19f9861b6bac28ce1ae7a108dde2a2b
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-03 16:02:42 +0300

    Implement generate_primes functionality

--- prime_numbers.py
+++ prime_numbers.py
@@ -10,3 +10,12 @@ def generate_primes(n) -> list:
     """
     if not isinstance(n, int):
         raise TypeError('Argument should be an integer')
+
+    primes = []
+    for num in range(2, n + 1):
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            primes.append(num)
+    return primes
